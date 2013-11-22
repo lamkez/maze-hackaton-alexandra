0d682ab5ec2343074e23f328fba67df39db8e43f
Kasper Damgaard <kasper.damgaard@alexandra.dk>
2013-11-22 14:55:35 +0100

    Added more security

--- js/lib/checkInput.js
+++ js/lib/checkInput.js
@@ -4,12 +4,34 @@
 **/
 
 function checkUserInput(text){
-    var p1 = /function\(/gi;
-    var p2 = /foo\(/gi;
+    var p1 = /move\(/gi;
+    var p2 = /canMoveFromPosition\(/gi;
+    var p3 = /getMovable\(/gi;
+    var p4 = /solveMaze\(/gi;
+
+    var internal_function = "Nope - Do not use internal functions";
     if(text.match(p1)){
+	alert(internal_function);
 	return false;
     }
     if(text.match(p2)){
+	alert(internal_function);
+	return false;
+    }
+    if(text.match(p3)){
+	alert(internal_function);
+	return false;
+    }
+    if(text.match(p4)){
+	alert(internal_function);
+	return false;
+    }
+
+    var setxPattern = /x\s*=/gi;
+    var setyPattern = /y\s*=/gi;
+
+    if(text.match(setxPattern) || text.match(setyPattern)){
+	alert("Nope - CHEATER.. Do not set x or y");
 	return false;
     }
 
@@ -20,7 +42,7 @@ function checkUserInput(text){
     }
 
     var positionPattern = /currentPos/ig;
-    if(text.match(positionPattern){
+    if(text.match(positionPattern)){
 	alert("Nope - CHEATER.. Do not try to change the position yourself");
 	return false;
     }
